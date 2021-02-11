90f3f896bb6c4a7140720f0bbf7a2ddb50a296f1
NerdyNarc <78885726+NerdyNarc@users.noreply.github.com>
2021-02-10 19:06:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+var name= prompt ("What is your name?")
+var color= prompt ("What is your favorite color")
+document.body.querySelector(".writer").innerHTML= "My name is " +name+ " and my favorite color is " +color
