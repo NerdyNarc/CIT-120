cd3f2e8bdffc320eeab6a628bbee5c62ff8ab027
NerdyNarc <78885726+NerdyNarc@users.noreply.github.com>
2021-02-28 12:24:19 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-var name= prompt ("What is your name?")
-var color= prompt ("What is your favorite color")
-document.body.querySelector(".writer").innerHTML= "My name is " +name+ " and my favorite color is " +color
+var containerEle = document.body.querySelector(".container");
+
+const number1 = prompt('Enter first number: ');
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+const number2 = prompt('Enter second number: ');
+
+let result;
+
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+containerEle.innerHTML= "The answer is "+result;
