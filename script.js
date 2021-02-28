899f8bdfa217c83536e35ebf35d3cd813bcd2f70
NerdyNarc <78885726+NerdyNarc@users.noreply.github.com>
2021-02-28 12:23:16 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-var containerEle = document.body.querySelector(".container");
-
-var num1 = Number(prompt("What is your bill?"));
-
-var num2 = Number((num1*.07)+num1);
-
-var num3 = (num2*.05)+num2;
-
-containerEle.innerHTML=num3;
+var name= prompt ("What is your name?")
+var color= prompt ("What is your favorite color")
+document.body.querySelector(".writer").innerHTML= "My name is " +name+ " and my favorite color is " +color
