2cccc4977364b87fb3e0fe5a0095b2dc821ff2c1
NerdyNarc <78885726+NerdyNarc@users.noreply.github.com>
2021-03-07 20:05:47 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-var containerEle = document.body.querySelector(".container");
+var dragonHP = 10;
 
-const number1 = prompt('Enter first number: ');
-const operator = prompt('Enter operator ( either +, -, * or / ): ');
-const number2 = prompt('Enter second number: ');
+var playerHP = 5;
+while (dragonHP > 0 || playerHP > 0) {
+  var playerDamage = prompt(
+    "You are attacked by a FireBreathing Dragon. How many hits will you attempt? (Between 1-5)"
+  );
 
-let result;
+  playerDamage = Math.floor(Math.random() * playerDamage + 1);
+  var dragonDamage = Math.floor(Math.random() * 2 + 1);
 
-if (operator == '+') {
-    result = number1 + number2;
-}
-else if (operator == '-') {
-    result = number1 - number2;
-}
-else if (operator == '*') {
-    result = number1 * number2;
-}
-else {
-    result = number1 / number2;
+  if (playerDamage < 1 || playerDamage > 5) {
+    alert("Damage must be between 1 and 5");
+  }
+
+  if (isNaN(playerDamage)) {
+    alert("Value entered is not a number.");
+    break;
+  }
+
+  dragonHP = dragonHP - playerDamage;
+  if (dragonHP <= 0) break;
+  playerHP = playerHP - dragonDamage;
+  if (playerHP <= 0) break;
 }
 
-containerEle.innerHTML= "The answer is "+result;
+if (playerHP <= 0) {
+  document.body.querySelector(".PlayerDeath").innerHTML =
+    "You have Died! The Dragon Wins!";
+}
+
+if (dragonHP <= 0) {
+  document.body.querySelector(".DragonDeath").innerHTML =
+    "You have slain the Dragon!";
+}
+
+document.body.querySelector("#playerHP").innerHTML = playerHP;
+
+document.body.querySelector("#dragonHP").innerHTML = dragonHP;
