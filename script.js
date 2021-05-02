9d72f44e070e6d81bdd817207129dd2fc82e7df9
NerdyNarc <78885726+NerdyNarc@users.noreply.github.com>
2021-05-02 18:17:50 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -5,13 +5,10 @@ var messageBox2 = document.body.querySelector(".message2");
 var notes = [];
 var priority = [];
 
-
 var list = [
   {
-    name: "coy"
-  },
-  {
-   name: "coolStudent123"
+    username: "cool",
+    password: "password"
   }
 ];
 
@@ -19,66 +16,77 @@ var i = 0;
 i < list.length;
 i++;
 document.body.querySelector(".submit").addEventListener("click", function () {
-  if (inputBox.value == list[i].name) {
-    contentEle.innerHTML = "";
-    alert("Login successful, redirecting you.");
-    var headEle = document.createElement("h1");
-    messageBox.innerHTML = "";
-    headEle.innerHTML = "View page - Create Notes Here";
-    contentEle.appendChild(headEle);
-    var noteEle = document.createElement("input");
-    var impEle = document.createElement("input");
-    noteEle.placeholder = "Note...";
-    var impEle = document.createElement("input");
-    impEle.placeholder = "Priority...";
-    contentEle.appendChild(noteEle);
-    contentEle.appendChild(impEle);
-    var noteButton = document.createElement("button");
-    noteButton.textContent = "Save note";
-    noteButton.addEventListener("click", function () {
-      if (impEle.value.length == 0) {
-        messageBox2.innerHTML = "";
-        messageBox2.innerHTML = "You must assign a priority to your note.";
+  for (i = 0; i < list.length; i++) {
+    var userBox = document.body.querySelector(".username");
+    var passBox = document.body.querySelector(".password");
+    if (
+      userBox.value == list[i].username &&
+      passBox.value == list[i].password
+    ) {
+      contentEle.innerHTML = "";
+      alert("Login successful, redirecting you.");
+      var headEle = document.createElement("h1");
+      messageBox.innerHTML = "";
+      headEle.innerHTML = "Main Page";
+      contentEle.appendChild(headEle);
+      var noteEle = document.createElement("input");
+      var impEle = document.createElement("input");
+      noteEle.placeholder = "Name...";
+      var impEle = document.createElement("input");
+      impEle.placeholder = "Grade...";
+      contentEle.appendChild(noteEle);
+      contentEle.appendChild(impEle);
+      var noteButton = document.createElement("button");
+      var viewButton = document.createElement("button");
+      viewButton.textContent = "View Grade"
+      noteButton.textContent = "Enter Grade";
+      noteButton.addEventListener("click", function () {
+        if (impEle.value.length == 0) {
+          messageBox2.innerHTML = "";
+          messageBox2.innerHTML = "You must assign a grade to your name";
+        } 
+        if (isNaN(impEle.value)) {
+          messageBox2.innerHTML = "";
+          messageBox2.innerHTML = "Value entered for Grade was not a valid";
+        } else {
+          priority.push(impEle.value);
+          renderNote();
+        }
+        if (noteEle.value.length <= 1) {
+          messageBox.innerHTML = "";
+          messageBox.innerHTML =
+            "Your note input must be longer than 1 character.";
+        } else {
+          notes.push(noteEle.value);
+          renderNote();
+        }
+      });
+      function renderNote() {
+        document.body.querySelector(".note").innerHTML = "";
+        document.body.querySelector(".import").innerHTML = "";
+        for (var x = 0; x < notes.length; x++) {
+          messageBox.innerHTML = "";
+          messageBox2.innerHTML = "";
+          var itmEle = document.createElement("h3");
+          itmEle.innerHTML = "Name: " + notes[x] + "     Grade: " + priority[x];
+          document.body.querySelector(".note").appendChild(itmEle);
+        }
       }
-      if (isNaN(impEle.value)) {
-        messageBox2.innerHTML = "";
-        messageBox2.innerHTML = "Value entered for Priority was not a number.";
-      } else {
-        priority.push(impEle.value);
-        renderNote();
-      }
-      if (noteEle.value.length <= 1) {
-        messageBox.innerHTML = "";
-        messageBox.innerHTML =
-          "Your note input must be longer than 1 character.";
-      } else {
-        notes.push(noteEle.value);
-        renderNote();
-      } 
-    });
-    function renderNote() {
-      document.body.querySelector(".note").innerHTML = "";
-      document.body.querySelector(".import").innerHTML = "";
-      for (var x = 0; x < notes.length; x++) {
-        messageBox.innerHTML = "";
-        messageBox2.innerHTML = "";
-        var itmEle = document.createElement("h3");
-        itmEle.innerHTML = "Note: " + notes[x] + "     Priorty: " + priority[x];
-        document.body.querySelector(".note").appendChild(itmEle);
-      }
+      contentEle.appendChild(noteButton);
+    } else {
+      messageBox.innerHTML = "";
+      messageBox.innerHTML = "Username not found.";
     }
-    contentEle.appendChild(noteButton);
-  } else {
-    messageBox.innerHTML = "";
-    messageBox.innerHTML = "Username not found.";
-  }
-  if (inputBox.value.length <= 2) {
-    messageBox.innerHTML = "";
-    messageBox.innerHTML = "Username too few characters.";
-  }
+    if (userBox.value !== list.username) {
+      messageBox.innerHTML = "";
+      messageBox.innerHTML = "Username is Incorrect";
+      return;
+    }
 
-  if (inputBox.value.length >= 15) {
-    messageBox.innerHTML = "";
-    messageBox.innerHTML = "Username too many characters.";
+    if (passBox.value !== list.password) {
+      messageBox.innerHTML = "";
+      messageBox.innerHTML = "Password is Incorrect";
+      return;
+    }
   }
 });
